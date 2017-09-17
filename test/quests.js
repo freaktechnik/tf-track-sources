63ca389995a8038cb5b96a94f63b2529dfaceba0
Martin Giger <martin@humanoids.be>
2017-09-17 02:13:23 +0200

    Implement all quest tests

--- test/quests.js
+++ test/quests.js
@@ -4,6 +4,14 @@ import schema from '../schemas/quests.json';
 import tracks from '../data/tracks.json';
 import quests from '../data/quests.json';
 
+const getQuest = (name) => quests.find((q) => q.name === name),
+    requiredQuests = (quest) => quest.requirements.filter((r) => r.type === "quest").map((r) => r.name),
+    requiresName = (reqQuests, name) => reqQuests.some((q) => q === name),
+    checkQuest = (questName, name) => {
+        const reqQuests = requiredQuests(getQuest(questName));
+        return !requiresName(reqQuests, name) && reqQuests.every((q) => checkQuest(q, name));
+    };
+
 test('Quests comply to the schema', (t) => {
     const ajv = new Ajv();
 
@@ -32,5 +40,22 @@ test('Tracks exist', (t) => {
     }
 });
 
-test.todo('No circular quests');
-test.todo('Tracks rewarded after first use');
+test('No circular quests', (t) => {
+    for(const quest of quests) {
+        t.true(checkQuest(quest.name, quest.name));
+    }
+});
+
+test('Tracks rewarded after first use', (t) => {
+    //TODO don't make this test depend on the order of quests in the array. i.e. build the quest tree.
+    let awardedTracks = new Set();
+    for(const quest of quests) {
+        const requiredTracks = quest.requirements.filter((r) => r.type === "track");
+        for(const track of requiredTracks) {
+            if(!awardedTracks.has(track.name)) {
+                t.true(quest.rewards.some((r) => r.type === track.type && r.name == track.name && r.amount >= 1));
+                awardedTracks.add(track.name);
+            }
+        }
+    }
+});
